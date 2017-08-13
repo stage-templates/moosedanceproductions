24a27680faa68801debf0633e14a6ec667aaab01
freddyroberts <froberts@lewiscommunications.com>
2017-08-13 14:29:34 -0500

    menu transition

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,9 @@
 /**
- * app.css
+ * app.js
  */
+
+import "classlist-polyfill";
+
+document.getElementById('menu').onclick = function(){
+  document.getElementsByClassName('header')[0].classList.toggle("is-visible");
+}
