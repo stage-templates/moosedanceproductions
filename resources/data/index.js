59525033a8a15ff8120d19bb259d2f73a4864561
freddyroberts <froberts@lewiscommunications.com>
2017-08-13 18:53:02 -0500

    add data for clients and awards

--- resources/data/index.js
+++ resources/data/index.js
@@ -7,5 +7,50 @@ module.exports = {
     "version": package.version || "",
     "environment": process.env.NODE_ENV || "development",
     "production": (process.env.NODE_ENV === "production"),
-	"bodyClass": "home-bg"
+	"clients": [
+		{
+			"logopath": "jose-logo.png"
+		},
+		{
+			"logopath": "microsoft-logo.png"
+		},
+		{
+			"logopath": "usa-today-logo.png"
+		},
+		{
+			"logopath": "hss-logo-web.png"
+		},
+		{
+			"logopath": "berman-braun-logo.png"
+		},
+		{
+			"logopath": "the-winslow-logo.png"
+		},
+		{
+			"logopath": "tawkers-logo.png"
+		},
+		{
+			"logopath": "completely-bare-logo.png"
+		},
+		{
+			"logopath": "cbr-logo.png"
+		},
+		{
+			"logopath": "mall-meds-logo.png"
+		}
+	],
+	"awards": [
+		{
+			"logopath": "shorty-logo.png"
+		},
+		{
+			"logopath": "the-webby-logo.png"
+		},
+		{
+			"logopath": "media-awards-logo.png"
+		},
+		{
+			"logopath": "abj_award.jpg"
+		}
+	]
 }
