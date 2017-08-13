65a3caa16de983c0a9a6950ac978192345d7282e
freddyroberts <froberts@lewiscommunications.com>
2017-08-13 17:55:15 -0500

    adjust bg images

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,3 +7,6 @@ import "classlist-polyfill";
 document.getElementById('menu').onclick = function(){
   document.getElementsByClassName('header')[0].classList.toggle("is-visible");
 }
+var url = window.location.pathname.replace('/', '').replace('.html', '');
+
+document.body.classList.add(url + '-bg');
