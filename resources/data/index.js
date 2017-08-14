f6e24d52e061c96753ae7c2b9c1d0d4db5cc5fb4
freddyroberts <froberts@lewiscommunications.com>
2017-08-13 19:33:13 -0500

    create data for work videos

--- resources/data/index.js
+++ resources/data/index.js
@@ -52,5 +52,49 @@ module.exports = {
 		{
 			"logopath": "abj_award.jpg"
 		}
+	],
+	"work": [
+		{
+			"client": "Jose Cuervo",
+			"title": "what is gangnam style?",
+			"description": "A video project for Jose Cuervo in which Pro Beach Volleyball players were asked: “Do you know what Gangnam Style is?” The results are… interesting.",
+			"video": "www.youtube.com/embed/wzgkQH0r-Fs",
+			"image": "gagnam.jpg"
+		},
+		{
+			"client": "Tasted",
+			"title": "Amazing Food Tricks",
+			"description": "An animated video for Rodale Publishing called “Amazing Food Tricks With Jill.",
+			"video": "www.youtube.com/embed/gsyMESkpvDs",
+			"image": "foodtricks.jpg"
+		},
+		{
+			"client": "Tasted",
+			"title": "dude food, with dinosaur bbq’s",
+			"description": "An episode of “Dude Food,” starring John Stage of the famed Dinosaur BBQ in New York, for Tasted Channel.",
+			"video": "www.youtube.com/embed/8ZbmjsB6s90",
+			"image": "dinobbq.jpg"
+		},
+		{
+			"client": "Jose Cuervo",
+			"title": "Impossible Vollyball Shot",
+			"description": "A video for Jose Cuervo, featuring one of the most difficult volleyball trick shots ever filmed.",
+			"video": "www.youtube.com/embed/HkL381Dk8A8",
+			"image": "Cuervo.jpg"
+		},
+		{
+			"client": "Berman Braun",
+			"title": "Woman V. Workout: Model Trains",
+			"description": "Woman V. Workout Episode: Can Model Lauren Berlingeri Handle FDNY Training?",
+			"video": "www.youtube.com/embed/NNN6zQdf6ZY",
+			"image": "firefighter.jpg"
+		},
+		{
+			"client": "MLB Fancave",
+			"title": "Big papi hugs new york ",
+			"description": "A viral video produced for MLB FanCave, in which Red Sox first baseman David Ortiz gives New Yorkers and Yankees fans a hug.",
+			"video": "www.youtube.com/embed/NNN6zQdf6ZY",
+			"image": "bigpapi.jpg"
+		}
 	]
 }
