3d844e9324a33278a6591fc8ddbbde558711eeec
freddyroberts <froberts@lewiscommunications.com>
2017-08-21 20:55:07 -0500

    update bodyClass check

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,6 +7,10 @@ import "classlist-polyfill";
 document.getElementById('menu').onclick = function(){
   document.getElementsByClassName('header')[0].classList.toggle("is-visible");
 }
-var url = window.location.pathname.replace('/', '').replace('.html', '');
-
-document.body.classList.add(url + '-bg');
+if (window.location.pathname === '/') {
+		document.body.classList.add('index-bg');
+} else {
+	var url = window.location.pathname.replace('/', '').replace('.html', '');
+	document.body.classList.add(url + '-bg');
+	console.log('wee');
+}
