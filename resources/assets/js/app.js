99174af5290c74bd9247a16da11e1349921ac724
freddyroberts <froberts@lewiscommunications.com>
2017-08-24 23:03:50 -0500

    start swiper

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,16 +1,25 @@
 /**
  * app.js
  */
-
+import "swiper";
 import "classlist-polyfill";
 
 document.getElementById('menu').onclick = function(){
-  document.getElementsByClassName('header')[0].classList.toggle("is-visible");
+	document.getElementsByTagName('body')[0].classList.toggle("is-visible");
 }
 if (window.location.pathname === '/') {
-		document.body.classList.add('index-bg');
+	document.body.classList.add('index-bg');
 } else {
 	var url = window.location.pathname.replace('/', '').replace('.html', '');
 	document.body.classList.add(url + '-bg');
-	console.log('wee');
 }
+
+
+var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30
+    });
