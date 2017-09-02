9a182b107ac6cc2c576ae041932aece583d9bc8f
freddyroberts <froberts@lewiscommunications.com>
2017-09-02 11:30:33 -0500

    add page not found class

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,6 +5,7 @@ import "swiper";
 import "classlist-polyfill";
 const doc = document;
 const classList = doc.body.classList;
+const url = window.location.pathname.replace('/', '').replace('.html', '');
 let routes = [
 	'/',
 	'about',
@@ -13,13 +14,22 @@ let routes = [
 	'index',
 	'work'
 ];
-let url = window.location.pathname.replace('/', '').replace('.html', '');
+
+let hasRoute = false;
+routes.find(function(el) {
+	if (el === url) {
+		hasRoute = true;
+		return;
+	}
+});
 
 doc.getElementById('menu').onclick = function(){
 	doc.getElementsByTagName('body')[0].classList.toggle("is-visible");
 }
 if (window.location.pathname === '/') {
 	classList.add('index-bg');
+} else if (!hasRoute) {
+		classList.add('page-not-found');
 } else {
 	classList.add(url + '-bg');
 }
